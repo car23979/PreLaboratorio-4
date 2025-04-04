6b15a4bb2440ae3b586f39c532bfaa4cca0fc21c
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-04 14:01:27 -0600

    Se corrigieron errore de llaves

--- PreLaboratorio_4/main.c
+++ PreLaboratorio_4/main.c
@@ -4,58 +4,51 @@
  * Created: 04/04/2025 09:30:46 a. m.
  * Author : David Carranza
  */ 
-
-// Encabezado
 #define F_CPU 16000000UL
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
-// Constantes
-#define INC_PIN    PB0   // Botón incremento (D8)
-#define DEC_PIN    PB1   // Botón decremento (D9)
+#define INC_PIN    PB0
+#define DEC_PIN    PB1
 
 volatile uint8_t contador = 0;
 
-// Puertos
 void configurar_puertos() {
-	DDRD = 0xFF;    // PD0-PD7 como salidas (LEDs)
-	DDRB &= ~((1 << INC_PIN) | (1 << DEC_PIN)); // PB0-PB1 como entradas
-	PORTB |= (1 << INC_PIN) | (1 << DEC_PIN);   // Activar pull-ups
+	DDRD = 0xFF;    // Todos los pines D como salidas
+	DDRB &= ~((1 << INC_PIN) | (1 << DEC_PIN)); // PB0 y PB1 como entradas
+	PORTB |= (1 << INC_PIN) | (1 << DEC_PIN);   // Pull-ups activados
 }
 
 void actualizar_leds() {
-	PORTD = contador; // Mostrar el contador directamente en PD0-PD7
+	PORTD = contador;
 }
 
-// Configuración Puertos
 int main() {
 	configurar_puertos();
 	uint8_t estado_inc = 1, estado_dec = 1;
-	
-	while(1) {
-	 // Leer estados de los botones (pull-up activado: 0 = presionado)
-	 uint8_t inc_actual = !(PINB & (1 << INC_PIN));
-	 uint8_t dec_actual = !(PINB & (1 << DEC_PIN));
-	 
-	 // Lógica antirrebote para incremento
-	 if (inc_actual && !estado_inc) {
-		 _delay_ms(20);
-		 if (!(PINB & (1 << INC_PIN))) {
-			 contador++;
-		 }
-	 }
-	 estado_inc = inc_actual
-	 
-	 // Lógica antirrebote para decremento
-	 if (dec_actual && !estado_dec) {
-		 _delay_ms(20);
-		 if (!(PINB & (1 << DEC_PIN))) {
-			 contador--;
-		 }
-	 }
-	 estado_dec = dec_actual;
 
-	 actualizar_leds(); // Actualizar LEDs
- }
- }
+	while(1) {
+		uint8_t inc_actual = !(PINB & (1 << INC_PIN));
+		uint8_t dec_actual = !(PINB & (1 << DEC_PIN));  // Variable ahora usada
+
+		// Lógica para incremento
+		if (inc_actual && !estado_inc) {
+			_delay_ms(20);
+			if (!(PINB & (1 << INC_PIN))) {
+				contador++;
+			}
+		}
+		estado_inc = inc_actual;
+
+		// Lógica para decremento (corregido)
+		if (dec_actual && !estado_dec) {  // Ahora usa las variables correctas
+			_delay_ms(20);
+			if (!(PINB & (1 << DEC_PIN))) {
+				contador--;
+			}
+		}
+		estado_dec = dec_actual;  // Variable ahora usada
+
+		actualizar_leds();
+	}
+}
