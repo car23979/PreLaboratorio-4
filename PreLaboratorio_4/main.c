53b56f8cf4a984d93a348cf061d77146502c4837
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-04 13:47:57 -0600

    Update main.c

--- PreLaboratorio_4/main.c
+++ PreLaboratorio_4/main.c
@@ -14,7 +14,6 @@
 // Constantes
 #define INC_PIN    PB0   // Botón incremento (D8)
 #define DEC_PIN    PB1   // Botón decremento (D9)
-#define DIGITO1    PB2   // Control dígito 1 (D10)
-#define DIGITO2    PB3   // Control dígito 2 (D11)
-#define ALARMA     PB4   // LED alarma (D12)
-#define POTENCIOMETRO 7  // ADC7 (A7
+
+
+// 
