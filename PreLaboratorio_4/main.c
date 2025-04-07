cc8915c22c1347ccd0368951c0d74100c8b48b63
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-07 17:07:27 -0600

    Se definieron constntes
    Se definieron más varaibles, para Los digitos, la alaram y el potenciometro

--- PreLaboratorio_4/main.c
+++ PreLaboratorio_4/main.c
@@ -11,8 +11,12 @@
 #include <util/delay.h>
 
 // Constantes
-#define INC_PIN    PB0
-#define DEC_PIN    PB1
+#define INC_PIN    PB0	 // Botón Incremento (D8)
+#define DEC_PIN    PB1	 // Botón Decremento (D9)
+#define DIGITO1    PB2   // Control dígito 1 (D10)
+#define DIGITO2    PB3   // Control dígito 2 (D11)
+#define ALARMA     PB4   // LED alarma (D12)
+#define POTENCIOMETRO 7  // ADC7 (A7)
 
 volatile uint8_t contador = 0;
 
