92742666f6a3896dabe96fdd21dc45ba2a6b40ca
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-07 16:52:52 -0600

    Pruebas

--- PreLaboratorio_4/main.c
+++ PreLaboratorio_4/main.c
@@ -1,18 +1,22 @@
 /*
  * PreLaboratorio_4.c
  *
- * Created: 04/04/2025 09:30:46 a. m.
+ * Created: 03/04/2025 09:30:46 a. m
  * Author : David Carranza
  */ 
+
+// Encabezado
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Constantes
 #define INC_PIN    PB0
 #define DEC_PIN    PB1
 
 volatile uint8_t contador = 0;
 
+// Puertos
 void configurar_puertos() {
 	DDRD = 0xFF;    // Todos los pines D como salidas
 	DDRB &= ~((1 << INC_PIN) | (1 << DEC_PIN)); // PB0 y PB1 como entradas
